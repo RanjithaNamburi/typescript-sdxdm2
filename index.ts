b4bf11a854a2749d609996e18db970a887c31c6a
RanjithaNamburi <namburiranjitha@gmail.com>
2021-06-19 20:47:46 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -3,6 +3,27 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>
+                    `;
+let resistor: any = [
+  'Black',
+  'Brown',
+  'Red',
+  'Orange',
+  'Yellow',
+  'Green',
+  'Blue',
+  'Violet',
+  'Grey',
+  'White'
+];
 
-console.log('test typescript');
+function twoFer(name: string) {
+  return 'one for ' + name + ' one for me';
+}
+
+console.log('Ranjitha Learning Typescript');
+
+console.log(twoFer('Ranjitha'));
+
+//console.log(resistor[4]);
